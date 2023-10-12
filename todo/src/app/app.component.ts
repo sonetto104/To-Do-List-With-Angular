cd5fdcd825c6b9e0045b6338bf02af98bf206665
sonetto104 <peter_regan88@yahoo.com>
2023-10-12 14:13:37 +0000

    add styling to item component

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Item } from "./item";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  templateUrl: "/app.component.html",
+  styleUrls: ["/app.component.css"],
 })
+
 export class AppComponent {
   title = "todo";
 
